573344d9842b3781409c5795a9bbb9e42eb58931
triss90405 <tw@d3sign.dk>
2015-04-04 00:42:36 +0200

    About section

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="author" content="Tristan White">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="icon" type="image/ico" href="favicon.ico">
 </head>
 <body>
@@ -18,10 +19,12 @@
                     <a href="index.php" class="logo">&lt; / &gt;</a>
                 </div>
                 <div class="col-xs-11">
-                    <a href="#home">Home</a>
-                    <a href="#about">About</a>
-                    <a href="#work">Work</a>
-                    <a href="#testimonials">Clients</a>
+                    <div class="menu">
+                        <a href="#about">About Me</a>
+                        <a href="#work">Work</a>
+                        <a href="#clients">Clients</a>
+                        <a href="#contact">Contact</a>
+                    </div>
                 </div>
             </div>
         </div>       
@@ -39,6 +42,147 @@
             </div>
         </div>
     </header>
+
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-m-12 center-text">
+                    <h2>About Tristan</h2>
+                    <div class="social">
+                        <a href="https://twitter.com/Triss90" target="_target" title="Tristans Twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="https://github.com/" target="_target"  title="Tristans GitHub"><i class="fa fa-github-alt"></i></a>
+                        <a href="https://www.facebook.com/tristanoliver.mosejensenwhite" target="_target"  title="Tristans Facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="https://dk.linkedin.com/in/tristanoliverwhite/en" target="_target"  title="Tristans LinkedIn"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                    <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong> Consequuntur voluptatum neque inventore molestiae assumenda voluptate eaque laboriosam aliquam consequatur maxime hic cum repellendus, soluta saepe, modi reprehenderit expedita dolore officiis.</p>
+                </div>
+            </div>
+
+            <div class="row skills">
+                        
+                <div class="col-m-6">
+                    <div class="skill-label">Communication</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">Organization</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">Learning</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">Planning</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">Photography</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">Visual Design</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">UX Design</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-m-6">
+                    <div class="skill-label">Programming</div>
+                    <div class="skill-icons">
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span class="active"></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
     <footer>
         <!--JS includes-->

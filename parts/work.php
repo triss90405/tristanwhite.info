ad6cec3605d87098b61fdbee0057f8017fa156fe
triss90405 <tw@d3sign.dk>
2015-04-05 07:41:29 +0200

    Changes to Work section

--- parts/work.php
+++ parts/work.php
@@ -7,52 +7,28 @@
 		</div>
 		
 		<div class="row projects">
-			<div class="col-m-3 col-s-6">
+			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">D3sign</div>
 					<img src="img/projects/prj1/thumb.png" class="sceletus-img" alt="d3sign">
 				</div>
 			</div>
-			<div class="col-m-3 col-s-6">
+			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">Sceletus</div>
 					<img src="img/projects/prj2/thumb.png" class="sceletus-img" alt="sceletus">
 				</div>
 			</div>
-			<div class="col-m-3 col-s-6">
+			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">Dittes Dilemma</div>
 					<img src="img/projects/prj3/thumb.png" class="sceletus-img" alt="dittesdilemma">
 				</div>
 			</div>
-			<div class="col-m-3 col-s-6">
+			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">Web Assets</div>
 					<img src="img/projects/prj4/thumb.png" class="sceletus-img" alt="web-assets">
-				</div>
-			</div>
-			<div class="col-m-3 col-s-6">
-				<div class="prj-item">
-					<div class="prj-overlay">Placeholder</div>
-					<img src="http://placehold.it/800x500" class="sceletus-img" alt="">
-				</div>
-			</div>
-			<div class="col-m-3 col-s-6">
-				<div class="prj-item">
-					<div class="prj-overlay">Placeholder</div>
-					<img src="http://placehold.it/800x500" class="sceletus-img" alt="">
-				</div>
-			</div>
-			<div class="col-m-3 col-s-6">
-				<div class="prj-item">
-					<div class="prj-overlay">Placeholder</div>
-					<img src="http://placehold.it/800x500" class="sceletus-img" alt="">
-				</div>
-			</div>
-			<div class="col-m-3 col-s-6">
-				<div class="prj-item">
-					<div class="prj-overlay">placeholder</div>
-					<img src="http://placehold.it/800x500" class="sceletus-img" alt="">
 				</div>
 			</div>
 		</div>

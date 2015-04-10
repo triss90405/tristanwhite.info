8cd3a0bec5a30356a65c7cbd305f500643fcb90b
triss90405 <tw@d3sign.dk>
2015-04-10 07:42:58 +0200

    Adding mail script

---
+++ mail.php
@@ -0,0 +1,18 @@
+<meta http-equiv="refresh" content="1;index.php" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<?php
+
+$mail = addslashes($_POST[mail]);
+$name = addslashes($_POST[name]);
+$msg = addslashes($_POST[msg]);
+
+$recipient = "tw@d3sign.dk";
+$subject = "Message from tomjw.dk - " . $name;
+$msg = $msg;
+
+$header  = "MIME-Version: 1.0" . "\r\n";
+$header .= "Content-type: text/html; charset='UTF-8'" . "\r\n";
+$header .= "from:" . $mail;
+
+mail($recipient, $subject, $msg, $header);
+?>

a154d2ceb81f9eccf3fd96769cfbab9f84d93db1
triss90405 <tw@d3sign.dk>
2015-04-10 18:22:35 +0200

    Changes to Client and Work

--- parts/clients.php
+++ parts/clients.php
@@ -5,8 +5,9 @@
 			<div class="col-m-4">
 				<img src="/img/clients/heidi.png" class="sceletus-img cirlce" alt="Heidi Ryghseter" />
 				<div class="text">
-					<p><i class="fa fa-quote-left"></i> Jeg har haft fornøjelsen af at samarbejde med Tristan vedrørende etableringen af min hjemmeside. <br>
-					Tristan er meget hård arbejdende og har været dygtig til at finde de rigtige tekniske løsninger som passer til mig og mine behov. <i class="fa fa-quote-right"></i></p>
+					<p><i class="fa fa-quote-left"></i> 
+					Tristan is very hardworking and has been adept at finding the right technical solutions that fit me and my needs.
+					<i class="fa fa-quote-right"></i></p>
 					<em>- Heidi Ryghseter</em>
 				</div>
 			</div>
@@ -14,8 +15,9 @@
 			<div class="col-m-4">
 				<img src="/img/clients/henriette.png" class="sceletus-img cirlce" alt="Henriette Knak" />
 				<div class="text">
-					<p><i class="fa fa-quote-left"></i> Samarbejdet med Tristan White har været rigtigt godt. Tristan er proffesionel, med masser af gode idéer og tiltag, som kun har forbedret desiget af min hjemmeside.
-					<br>Jeg har været yderst tilfreds, og vil gerne anbefale Tristan, til andre der skal have lavet hjemmesider. <i class="fa fa-quote-right"></i></p>
+					<p><i class="fa fa-quote-left"></i>
+					I have been extremely satisfied and would like to recommend Tristan to anyone in need of a websites.
+					<i class="fa fa-quote-right"></i></p>
 					<em>- Henriette Knak</em>
 				</div>
 			</div>
@@ -23,8 +25,9 @@
 			<div class="col-m-4">
 				<img src="/img/clients/kurt.png" class="sceletus-img cirlce" alt="Kurt Mikkelsen" />
 				<div class="text">
-					<p><i class="fa fa-quote-left"></i> Jeg har været særdeles tilfreds med Tristans arbejde. Han har været dygtig til at omsætte mine tanker og ideer til konkrete og enkle løsninger.
-					<br>Løsninger som jeg har været yderst tilfreds med både med hensyn til funktionalitet og layout. <i class="fa fa-quote-right"></i></p>
+					<p><i class="fa fa-quote-left"></i>
+					I am extremely pleased with my website, both in terms of functionality and layout. It does everything I need and more!
+					<i class="fa fa-quote-right"></i></p>
 					<em>- Kurt Mikkelsen</em>
 				</div>
 			</div>
--- parts/work.php
+++ parts/work.php
@@ -7,30 +7,43 @@
 		</div>
 		
 		<div class="row projects">
+
+			<a href="http://d3sign.dk" target="_target">
 			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">D3sign</div>
 					<img src="img/projects/prj1/thumb.png" class="sceletus-img" alt="d3sign">
 				</div>
 			</div>
+			</a>
+			
+			<a href="http://sceletus.net" target="_target">
 			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">Sceletus</div>
 					<img src="img/projects/prj2/thumb.png" class="sceletus-img" alt="sceletus">
 				</div>
 			</div>
+			</a>
+			
+			<a href="http://dittesdilemma.dk" target="_target">
 			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">Dittes Dilemma</div>
 					<img src="img/projects/prj3/thumb.png" class="sceletus-img" alt="dittesdilemma">
 				</div>
 			</div>
+			</a>
+
+			<a href="http://web-assets.net" target="_target">
 			<div class="col-m-4 col-s-6">
 				<div class="prj-item">
 					<div class="prj-overlay">Web Assets</div>
 					<img src="img/projects/prj4/thumb.png" class="sceletus-img" alt="web-assets">
 				</div>
 			</div>
+			</a>
+
 		</div>
 
 	</div>

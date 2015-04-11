0fe6b7e047785cd824ec1d46d2a8fb2e16cf9f59
triss90405 <tw@d3sign.dk>
2015-04-11 22:21:16 +0200

    Updating mail form

--- parts/contact.php
+++ parts/contact.php
@@ -5,9 +5,9 @@
 				<h2>Contact Me</h2>
 
 				<form method="post" action="mail.php"> 
-					<input type="email" name="mail" placeholder="Your E-mail">
-					<input type="text" name="name" placeholder="Your Name">
-					<textarea name="msg" placeholder="Your Message"></textarea><br>
+					<input type="email" name="mail" required placeholder="Your E-mail">
+					<input type="text" name="name" required placeholder="Your Name">
+					<textarea name="msg" required placeholder="Your Message"></textarea><br>
 					<input type="submit" value="Send Message"/>
 				</form>
 

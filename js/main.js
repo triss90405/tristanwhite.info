8bc538806fcbd412ffa43df56e771fd7627aad40
triss90 <oliver.tristan@gmail.com>
2015-06-01 12:11:54 +0200

    Adding email suggestion script

--- js/main.js
+++ js/main.js
@@ -42,6 +42,44 @@ $(document).ready(function () {
         });
     });
 
+    // Email auto suggestion
+    var email = document.querySelector('.email'),
+    auto = document.querySelector('.autosuffix');
+    var popularEmails = ['gmail.com', 'googlemail.com', 'hotmail.com', 'yahoo.com', 'msn.com', 'aol.com'];
+    email.addEventListener('keyup', function() {
+        auto.innerHTML = '';
+        if(email.value.match('@')) {
+            var afterAt = email.value.substring(email.value.indexOf('@') + 1, email.value.length);
+            var popularEmailsSub = [];
+            for(var l = 0; l < popularEmails.length; l++) {
+                popularEmailsSub.push(popularEmails[l].substring(0, afterAt.length))
+            }
+            if(afterAt === '') {
+                for(var i = 0; i < popularEmails.length; i++) {
+                    auto.innerHTML += '<li>' + email.value + popularEmails[i] + '</li>';
+                }
+            }
+            else if(!(afterAt === '')) {
+                var matchedEmails = [];
+                for(var k = 0; k < popularEmails.length; k++) {
+                    if(popularEmailsSub[k].match(afterAt)) {
+                        matchedEmails.push(popularEmails[k]);                    
+                    }
+                }
+                for(var i = 0; i < matchedEmails.length; i++) {
+                    auto.innerHTML += '<li>' + email.value.substring(0, email.value.indexOf('@')) + '@' + matchedEmails[i] + '</li>';
+                }
+            }
+            var autoItems = document.querySelectorAll('.autosuffix li');
+            for(var j = 0; j < autoItems.length; j++) {
+                autoItems[j].addEventListener('click', function() {
+                    email.value = this.textContent;
+                    auto.innerHTML = '';
+                });
+            }
+        }
+    });
+
     //Mobile menu
     $('.menu-btn').click(function () {
         $('.menu').toggleClass('show-menu');
--- parts/contact.php
+++ parts/contact.php
@@ -4,7 +4,10 @@
 			<div class="col-m-12 center-text">
 				<h2>Contact Me</h2>
 				<form method="post" action="" id="contactForm"> 
-					<input type="email" id="mail" name="mail" required placeholder="Your E-mail">
+					<div class="mail-wrapper">
+						<input type="email" class="email" id="mail" name="mail" required placeholder="Your E-mail">
+						<ul class="autosuffix"></ul>
+					</div>
 					<input type="text" id="name" name="name" required placeholder="Your Name">
 					<textarea name="msg" id="msg" rows="7" required placeholder="Your Message"></textarea><br>
 					<input type="submit" id="send" value="Send Message"/>

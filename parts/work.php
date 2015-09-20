42b1e3ad8a94d281688565c603c952e3537dfcdf
triss90405 <tw@d3sign.dk>
2015-09-20 07:27:30 +0200

    Updating work section

--- parts/work.php
+++ parts/work.php
@@ -5,74 +5,47 @@
 				<h2>Recent work</h2>
 			</div>
 		</div>
-		
-		<div class="row projects">
-			
-			<a href="http://xn--holmerundhjfestuge-p4b.dk/" target="_target">
-			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
-				<div class="prj-item">
-					<div class="prj-overlay">Holme-Rundhøjfestuge</div>
-					<img src="img/projects/prj3/thumb.png" class="sceletus-img" alt="Holmerundhøjfestuge">
-				</div>
-			</div>
-			</a>
 
-			<a href="http://www.packshotcph.com/" target="_target">
-			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
-				<div class="prj-item">
-					<div class="prj-overlay">Packshot CPH</div>
-					<img src="img/projects/prj8/thumb.png" class="sceletus-img" alt="PackshotCPH">
-				</div>
-			</div>
-			</a>
+		<div class="row projects">
 
 			<a href="http://www.annettepetersen.dk/" target="_target">
 			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
 				<div class="prj-item">
 					<div class="prj-overlay">Annette Petersen</div>
-					<img src="img/projects/prj7/thumb.png" class="sceletus-img" alt="Annette_Petersen">
-				</div>
-			</div>
-			</a>
-			
-			<a href="http://sceletus.net" target="_target">
-			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
-				<div class="prj-item">
-					<div class="prj-overlay">Sceletus</div>
-					<img src="img/projects/prj2/thumb.png" class="sceletus-img" alt="sceletus">
+					<img src="img/projects/prj1/thumb.png" class="sceletus-img" alt="Annette_Petersen">
 				</div>
 			</div>
 			</a>
 
-			<a href="http://web-assets.net" target="_target">
+			<a href="http://xn--holmerundhjfestuge-p4b.dk/" target="_target">
 			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
 				<div class="prj-item">
-					<div class="prj-overlay">Web Assets</div>
+					<div class="prj-overlay">Holme-Rundhøjfestuge</div>
+					<img src="img/projects/prj2/thumb.png" class="sceletus-img" alt="Holmerundhøjfestuge">
+				</div>
+			</div>
+			</a>
+
+			<a href="http://sceletus.net" target="_target">
+			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
+				<div class="prj-item">
+					<div class="prj-overlay">Sceletus</div>
+					<img src="img/projects/prj3/thumb.png" class="sceletus-img" alt="sceletus">
+				</div>
+			</div>
+			</a>
+
+			<a href="http://ryghseter.dk" target="_target">
+			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
+				<div class="prj-item">
+					<div class="prj-overlay">Ryghseter</div>
 					<img src="img/projects/prj4/thumb.png" class="sceletus-img" alt="web-assets">
 				</div>
 			</div>
 			</a>
 
-			<a href="http://rundhoejskolen.skoleporten.dk/sp" target="_target">
-			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
-				<div class="prj-item">
-					<div class="prj-overlay">Rundhøjskolen</div>
-					<img src="img/projects/prj5/thumb.png" class="sceletus-img" alt="Rundhøjskolen">
-				</div>
-			</div>
-			</a>
-
-			<a href="http://hoejen-skole.skoleporten.dk/sp" target="_target">
-			<div class="col-l-3 col-m-4 col-s-6 col-xs-12">
-				<div class="prj-item">
-					<div class="prj-overlay">Højen Skole</div>
-					<img src="img/projects/prj6/thumb.png" class="sceletus-img" alt="Højen_Skolen">
-				</div>
-			</div>
-			</a>
 
 		</div>
 
 	</div>
 </section>
-

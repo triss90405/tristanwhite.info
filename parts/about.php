c7479168d5e78b9cb61365945165cc74db77bf20
triss90405 <tw@d3sign.dk>
2015-09-20 06:43:06 +0200

    Updating social icons

--- parts/about.php
+++ parts/about.php
@@ -7,18 +7,18 @@
                     <a href="https://twitter.com/Triss90" target="_target" title="Tristans Twitter"><i class="fa fa-twitter"></i></a>
                     <a href="https://github.com/triss90405" target="_target"  title="Tristans GitHub"><i class="fa fa-github-alt"></i></a>
                     <a href="https://www.facebook.com/tristanoliver.mosejensenwhite" target="_target"  title="Tristans Facebook"><i class="fa fa-facebook"></i></a>
-                    <a href="https://dk.linkedin.com/in/tristanoliverwhite/en" target="_target"  title="Tristans LinkedIn"><i class="fa fa-linkedin"></i></a>
+                    <a href="http://codepen.io/triss90/" target="_target"  title="Tristans CodePen"><i class="fa fa-codepen"></i></a>
                 </div>
-                <p><strong>Hi! My name is Tristan White, I'm a full-stack developer.</strong> 
-                I was born and raised in a small town in Denmark. Today I am proud to call myself a full-stack web developer. 
-                I love making beautiful, yet practical websites and applications. 
-                I would enjoy the opportunity to collaborate on your next project, so drop me an 
+                <p><strong>Hi! My name is Tristan White, I'm a full-stack developer.</strong>
+                I was born and raised in a small town in Denmark. Today I am proud to call myself a full-stack web developer.
+                I love making beautiful, yet practical websites and applications.
+                I would enjoy the opportunity to collaborate on your next project, so drop me an
                 <a href="mailto:tw@d3sign.dk?Subject=Hi%20Tristan" target="_top">email</a> and lets get to work!</p>
             </div>
         </div>
 
         <div class="row skills">
-                    
+
             <div class="col-m-6">
                 <div class="skill-label">Communication</div>
                 <div class="skill-icons">
--- parts/footer.php
+++ parts/footer.php
@@ -2,18 +2,15 @@
     	<div class="container">
     		<div class="row">
     			<div class="col-s-6">
-    				<p>Made with
-		            <svg class="heart" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="92.515px" height="73.161px" viewBox="0 0 92.515 73.161" enable-background="new 0 0 92.515 73.161" xml:space="preserve"><g><path fill="#010101" d="M82.32,7.888c-8.359-7.671-21.91-7.671-30.271,0l-5.676,5.21l-5.678-5.21c-8.357-7.671-21.91-7.671-30.27,0c-9.404,8.631-9.404,22.624,0,31.255l35.947,32.991L82.32,39.144C91.724,30.512,91.724,16.52,82.32,7.888z"></path></g></svg> 
-		            by <a href="https://github.com/triss90405" target="_target">Tristan White</a></p>
 		            <p>Built on <a href="http://sceletus.net" target="_target">sceletus</a></p>
-		            &copy;Copyright <?php echo date("Y") ?> - <a href="http://d3sign.dk/">d3sign</a>
+		            &copy;Copyright <?php echo date("Y") ?> - <a href="https://github.com/triss90405">Tristan White</a>
     			</div>
     			<div class="col-s-6">
     				<div class="social">
 	                    <a href="https://twitter.com/Triss90" target="_target" title="Tristans Twitter"><i class="fa fa-twitter"></i></a>
 	                    <a href="https://github.com/triss90405" target="_target"  title="Tristans GitHub"><i class="fa fa-github-alt"></i></a>
 	                    <a href="https://www.facebook.com/tristanoliver.mosejensenwhite" target="_target"  title="Tristans Facebook"><i class="fa fa-facebook"></i></a>
-	                    <a href="https://dk.linkedin.com/in/tristanoliverwhite/en" target="_target"  title="Tristans LinkedIn"><i class="fa fa-linkedin"></i></a>
+                       <a href="http://codepen.io/triss90/" target="_target"  title="Tristans CodePen"><i class="fa fa-codepen"></i></a>
 	                </div>
     			</div>
     		</div>
@@ -21,7 +18,7 @@
         <!--JS includes-->
         <script src="js/jquery-2.1.3.min.js"></script>
         <script src="js/min/main-min.js"></script>
-        
+
         <!--Google Analytics-->
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){

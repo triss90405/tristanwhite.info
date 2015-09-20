fb49947d8ae067f1658c2008d8f4f69b49100352
triss90405 <tw@d3sign.dk>
2015-09-20 06:36:24 +0200

    Updating contact section

--- parts/contact.php
+++ parts/contact.php
@@ -1,18 +1,11 @@
+<a href="mailto:oliver.tristan@gmail.com">
 <section id="contact">
 	<div class="container">
 		<div class="row">
 			<div class="col-m-12 center-text">
-				<h2>Contact Me</h2>
-				<form method="post" action="" id="contactForm"> 
-					<div class="mail-wrapper">
-						<input type="email" class="email" id="mail" name="mail" required placeholder="Your E-mail">
-						<ul class="autosuffix"></ul>
-					</div>
-					<input type="text" id="name" name="name" required placeholder="Your Name">
-					<textarea name="msg" id="msg" rows="7" required placeholder="Your Message"></textarea><br>
-					<input type="submit" id="send" value="Send Message"/>
-				</form>
+				<h1>oliver.tristan@gmail.com</h1>
 			</div>
 		</div>
 	</div>
 </section>
+</a>
